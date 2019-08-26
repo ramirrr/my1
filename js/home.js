f1eef8a470e85b15aa2afe8136a5031835552d6a
ramirrr <ranirani98t@gmail.com>
2019-08-26 12:56:00 +0300

    r

--- js/home.js
+++ js/home.js
@@ -55,3 +55,113 @@ for(lu2 in lop2){
 }
 //راح يطبع قيم المصفةفة على عددها بشكل اندكسات بلتسلسل
 
+let asma =["mrda","amgd","jafer","wail"]
+asma.forEach(n => console.log(n))
+//هذي دالة جاهزة تطبع كل المصفوفة
+let kreta=asma.map(n => "<li>"+n+"</li>")
+//console.log(kreta)
+//هذي الدالة الجاهزة وضيفتها تحشر الشي وصط اشياء
+for(lu2 in kreta){
+    console.log(kreta[lu2])
+}
+//طباعة القائمة المعدلة
+
+let mwthfen=[
+    {name:"hanza",age:32,selary:300},
+    {name:"aya",age:62,selary:3000},
+    {name:"nor",age:12,selary:700},
+    {name:"zina",age:72,selary:6000},
+]
+let kwar=mwthfen.filter(n => n.selary <3000).map(n =>"أسم الموضف:"+ n.name)
+//console.log(kwar)
+//هنا خاصية الفلتر استخدمناها بلبداية سوينة قاعدة بيانات بعدي بلفلتر خلينة الشرط انو السالاري اقل من 3000
+for(lu3 in kwar){
+    console.log(kwar[lu3])
+}
+//طباعة القائمة المعدلة
+
+class mostakdm{
+    constructer(asm,amr,ident){
+        this.asm=asm
+        this.amr=amr
+        this.ident=ident
+        }
+    tsgel(us,pas){
+        if (us=="vd"&&pas=="123"){
+            console.log("مرحبا")
+        }else{
+            console.log("الرمز أو كلمة المرور غلط")
+        }
+    }
+    kroj(){
+        console.log("ثيملا")
+    }
+}
+let wrya=new mostakdm("wrya",65,4325423)
+console.log(wrya.ident)
+wrya.tsgel("fe","ef")
+wrya.kroj()
+//مثال علكلاس
+
+class moder extends mostakdm{
+    constructer(asm,amr,ident){
+        this.asm=asm
+        this.amr=amr
+        this.ident=ident
+        }
+}
+let omer=new moder("omer",33,8756654)
+omer.kroj()
+//مثال عن التوريث
+
+let j=async()=>{
+    let u=await fetch("https://my-json-server.typicode.com/typicode/demo/posts")
+    //هذي دالة فيتجر جاهزة
+    //الاويت تعني الانتضار وهية ضرورية لان يطول علما يجيب المعلومات من المكان الموقع الخارجي
+    let pos=await u.json()
+    pos.map(p => console.log("<h1>"+p.title+"</h1>"))
+}
+j()
+//هذا درس عن الجيسون
+
+let rsala= () =>{
+    alert("السلام عليكم")
+}
+rsala()
+//دالة الالارت تستخدم لطبع رسالة
+let ll=() =>{
+    document.querySelector(".one").innerHTML="computer"
+    //نختار اسم الكلاس الموجود بملف الاج تي ام ال ونخلي الكلام الجديد
+}
+ll()
+
+let dmg=() =>{
+    let jm=document.querySelector(".five").innerHTML
+    alert(jm)
+}
+dmg()
+//دمجنة اخر دالتين بهذي الدالة
+
+let trheb=() =>{
+    let fl=document.querySelector("#js2").value
+    alert("شلونك "+fl+"؟")
+}
+//دالة تتنفذ من ينضغط علزر ووضيفتها اظهار رسالة
+
+let tageer=()=>{
+    let fy=document.querySelector("#js4")
+    document.querySelector(".six").innerHTML=fy.value
+}
+//دالة تبديل النص
+
+let ano=document.querySelector("#js5")
+let ab=0
+let plus=()=>{
+    ano.innerHTML=ab++
+    //let fal=document.querySelector("#js5").value
+    //document.querySelector("#js5").innerHTML=fal+1
+}
+let min=()=>{
+    ano.innerHTML=ab--
+}
+
